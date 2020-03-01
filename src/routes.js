e71582b3145fe10b7ec378c2ff5aa9522e6ac9d7
irailtonreis <irailton@gmail.com>
2020-03-01 12:01:36 -0300

    Adicionaando validação adimin

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,10 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/users', UserController.store);
 
 export default routes;
